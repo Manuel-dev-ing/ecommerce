e26886550c6ce17aced98dc7053d0853dbad0ecb
Manuel Tamayo Montero <manueltamayo9765@gmail.com>
2026-02-11 21:12:57 -0600

    create dynamic access

--- resources/js/layouts/home-layout.tsx
+++ resources/js/layouts/home-layout.tsx
@@ -1,7 +1,15 @@
 import { MapPin, Search, ShoppingCart } from "lucide-react"
 import type { ReactNode } from "react"
+import buyprotected from '@/images/buyprotected.svg'
+
+import enviogratis from '@/images/envio-gratis.svg'
 import envioslogotipo from '@/images/envioslogotipo.webp'
+import heroImage from '@/images/hero-image.webp'
 import logotipo from '@/images/logo.png'
+import lowprice from '@/images/lowprice.svg'
+import registration from '@/images/registration.svg'
+import topsale from '@/images/topsale.svg'
+import vistorecientemente from '@/images/vistorecientemente.webp'
 
 type HomeLayoutProps = {
     children: ReactNode
@@ -10,7 +18,7 @@ type HomeLayoutProps = {
 export default function HomeLayout({children}:HomeLayoutProps) {
   return (
     <>
-        <header className=" bg-[#fee73a] h-24">
+        <header className="bg-[#fee73a] h-24">
             <nav className="max-w-7xl mx-auto p-2 grid grid-cols-6 gap-2">
                 <div className="text-center col-span-1 flex justify-start">
                     <img className="w-[134px] h-[36px] " src={logotipo} alt="logotipo" />
@@ -30,7 +38,7 @@ export default function HomeLayout({children}:HomeLayoutProps) {
                     </form>
 
                 </div>
-                <div className="text-center  col-span-2">
+                <div className="text-center flex justify-end col-span-2">
                     <img className="w-[325px] h-[38px] " src={envioslogotipo} alt="" />
                 </div>
                 <div className="text-center col-span-1 flex items-center">
@@ -85,7 +93,7 @@ export default function HomeLayout({children}:HomeLayoutProps) {
                         </li>
                     </ul>
                 </div>
-                <div className="text-center col-span-2 flex items-center gap-5">
+                <div className="text-center col-span-2 flex justify-end items-center gap-5">
                     <p className="text-xs">Crea tu cuenta</p>
                     <p className="text-xs">Ingresa</p>
                     <p className="text-xs">Mis compras</p>
@@ -95,10 +103,141 @@ export default function HomeLayout({children}:HomeLayoutProps) {
             </nav>
 
         </header>
+        <main className="">
+            <div className="w-full relative">
+                {/* section hero */}
+                <img className="block w-full h-[400px] object-cover" src={heroImage} alt="" />
+                <div className=" absolute bottom-0 left-0 w-full h-[30%] z-[3] bg-gradient-to-b from-transparent to-[#ebebeb]">
+                    <div className="max-w-7xl mx-auto">
+                        <div className="flex justify-between">
 
-        <main className="max-w-7xl mx-auto border-2 ">
-            {children}
+                            <div className="bg-white w-[183px] h-[300px] rounded-md py-3 px-3 flex flex-col items-center justify-between">
+                                <div>
+                                    <p className="font-medium">Envio gratis</p>
+                                    <a className="flex flex-col items-center" href="">
+                                        <img className="mt-4" src={enviogratis} alt="envio gratis" />
+                                        <p className="text-center text-sm mt-3 hover:text-blue-600">
+                                            Beneficio por ser tu primera compra
+                                        </p>
+
+                                    </a>
+                                </div>
+                                <a className="text-xs bg-blue-100 text-blue-800 font-medium me-2 px-5 py-0.5 rounded" href="">
+                                    Mostrar productos
+                                </a>
+                            </div>
+
+                            <div className="bg-white w-[183px] h-[300px] rounded-md py-3 px-3 flex flex-col items-center">
+                                <div>
+                                    <p className="font-medium">Visto recientemente</p>
+                                    <a className="flex flex-col items-center">
+                                        <img className="mt-4 w-[100px] h-[100px] object-cover" 
+                                            src={vistorecientemente} 
+                                            alt="envio gratis" 
+                                        />
+                                        <p className="line-clamp-2 text-sm mt-3 hover:text-blue-600">
+                                           Auriculares Inalámbricos Xiaomi Redmi Buds 6 Play Negro
+                                        </p>
+
+                                    </a>
+                                </div>
+                                <div className="w-full mt-3">
+                                    <div className="flex items-center gap-1">
+                                        <p className="font-normal text-lg">$290</p>
+                                        <p className="text-xs text-[#40a751]">51% OFF</p>
+                                    </div>
+                                    <p className="text-black text-sm">
+                                        <span className="text-[#40a751]">Envio gratis{' '}</span> 
+                                        por ser tu primera compra
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="bg-white w-[183px] h-[300px] rounded-md py-3 px-3 flex flex-col items-center justify-between">
+                                <div>
+                                    <p className="font-medium">Ingresa a tu cuenta</p>
+                                    <a className="flex flex-col items-center" href="">
+                                        <img className="mt-4 w-[100px] h-[100px] object-cover" 
+                                            src={registration} alt="envio gratis" />
+                                        <p className="text-center text-sm mt-3 hover:text-blue-600">
+                                            Disfruta de ofertas y compra sin limites
+                                        </p>
+
+                                    </a>
+                                </div>
+                                <a className="text-xs bg-blue-100 text-blue-800 font-medium me-2 px-5 py-0.5 rounded" href="">
+                                    Ingresa a tu cuenta
+                                </a>
+                            </div>
+
+                            <div className="bg-white w-[183px] h-[300px] rounded-md py-3 px-3 flex flex-col items-center justify-between">
+                                <div>
+                                    <p className="font-medium">Menos de $500</p>
+                                    <a className="flex flex-col items-center" href="">
+                                        <img className="mt-4 w-[100px] h-[100px] object-cover" 
+                                            src={lowprice} alt="envio gratis" />
+                                        <p className="text-center text-sm mt-3 hover:text-blue-600">
+                                            Descubre precios con precios bajos
+                                        </p>
+
+                                    </a>
+                                </div>
+                                <a className="text-xs bg-blue-100 text-blue-800 font-medium me-2 px-5 py-0.5 rounded" href="">
+                                    Ingresa a tu cuenta
+                                </a>
+                            </div>
+
+                            <div className="bg-white w-[183px] h-[300px] rounded-md py-3 px-3 flex flex-col items-center justify-between">
+                                <div>
+                                    <p className="font-medium">Mas vendidos</p>
+                                    <a className="flex flex-col items-center" href="">
+                                        <img className="mt-4 w-[100px] h-[100px] object-cover" 
+                                            src={topsale} 
+                                            alt="top sale" />
+                                        <p className="text-center text-sm mt-3 hover:text-blue-600">
+                                            Explora los productos que son tendencia
+                                        </p>
+
+                                    </a>
+                                </div>
+                                <a className="text-xs bg-blue-100 text-blue-800 font-medium me-2 px-5 py-0.5 rounded" href="">
+                                    Ir a Mas vendidos
+                                </a>
+                            </div>
+                            <div className="bg-white w-[183px] h-[300px] rounded-md py-3 px-3 flex flex-col items-center justify-between">
+                                <div>
+                                    <p className="font-medium">Compra protegida</p>
+                                    <a className="flex flex-col items-center" href="">
+                                        <img className="mt-4 w-[100px] h-[100px] object-cover" 
+                                            src={buyprotected} 
+                                            alt="top sale" />
+                                        <p className="text-center text-sm mt-3 hover:text-blue-600">
+                                            Puedes devolver tu compra gratis
+                                        </p>
+
+                                    </a>
+                                </div>
+                                <a className="text-xs bg-blue-100 text-blue-800 font-medium me-2 px-5 py-0.5 rounded" href="">
+                                    Como funciona
+                                </a>
+                            </div>
+
+
+                        </div>
+                        <div className="border border-red-600">
+                            {children}
+                        </div>        
+                    </div>
+
+                </div>
+
+            </div>
+            
+                
+                  
         </main>
+
+
     </>
   )
 }
