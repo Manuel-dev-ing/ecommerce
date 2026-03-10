7fb427f2923a6ae67a213f891a16ca7fa858a1a1
Manuel Tamayo Montero <manueltamayo9765@gmail.com>
2026-03-09 21:40:08 -0600

    create routes options and features

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FeatureController;
 use App\Http\Controllers\OptionsController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
@@ -18,7 +19,15 @@ Route::get('dashboard', function () {
 
 Route::get('/products', [ProductsController::class, 'index'])->name('products.index');
 
+// Options
 Route::get('/options', [OptionsController::class, 'index'])->name('options.index');
+Route::post('/options', [OptionsController::class, 'store'])->name('options.store');
+Route::delete('/options/{id}', [OptionsController::class, 'destroy'])->name('options.destroy');
+Route::put('/options/{id}', [OptionsController::class, 'update'])->name('options.update');
 
+// Features
+Route::post('/features', [FeatureController::class, 'store'])->name("features.store");
+Route::delete('/features/{id}', [FeatureController::class, 'destroy'])->name("features.destroy");
+Route::put('/features/{id}', [FeatureController::class, 'update'])->name("features.update");
 
 require __DIR__.'/settings.php';
